9f2d24475e18fe44b41926a3276eb5d4589c7001
Person Person <thisemail@mailtothis.com>
2015-06-26 13:13:35 -0500

    Create bancheck.lua

---
+++ ULX-Plugins/bancheck.lua
@@ -0,0 +1,12 @@
+function ulx.bancheck (calling_ply, target_ply)
+	PrintMessage(HUD_PRINTTALK, "Check console for SteamID")
+	PrintMessage(HUD_PRINTCONSOLE, "The player's SteamID is " .. target_ply:SteamID())
+	calling_ply:SendLua([[gui.OpenURL('http://gflclan.com/gmodbans/index.php?p=banlist&advSearch=]]..target_ply:SteamID()..[[&advType=steamid')]])
+end
+
+local bancheck = ulx.command(CATEGORY_NAME, "ulx bancheck", ulx.bancheck)
+bancheck:addParam{ type=ULib.cmds.PlayerArg }
+bancheck:defaultAccess(ULib.ACCESS_ALL)
+bancheck:help ("Displays the player's SteamID")
+
+-- This may or may not have been stolen from somebody who I may or may not hate
