42bfaa488d138a09ed092d8bfbcfe6fb479cbed7
Person Person <thisemail@mailtothis.com>
2015-08-01 16:17:34 -0500

    changed to admin only

--- bancheck.lua
+++ bancheck.lua
@@ -6,7 +6,7 @@ end
 
 local bancheck = ulx.command(CATEGORY_NAME, "ulx bancheck", ulx.bancheck, "!bancheck")
 bancheck:addParam{ type=ULib.cmds.PlayerArg }
-bancheck:defaultAccess(ULib.ACCESS_ALL)
+bancheck:defaultAccess(ULib.ACCESS_ADMIN)
 bancheck:help ("Displays the player's SteamID")
 
 -- This may or may not have been stolen from somebody who I may or may not hate
